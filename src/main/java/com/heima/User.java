becd353a402da40f7ec1cbe2f013d463eb48e90c
wangfeiiqangok <wangfeiqiangok@126.com>
2020-06-26 22:28:41 +0800

    rwgrdf

--- src/main/java/com/heima/User.java
+++ src/main/java/com/heima/User.java
@@ -6,8 +6,8 @@ package com.heima;
  * @author Administrator 2020/06/26 下午 22:01
  */
 public class User {
-    private String username;//用户名
-    private int age;
-    private String password;
+    private String hrnsjyusername;//用户名
+    private int agggggggggggggggge;
+    private String passwghhhhhhhhhhhhord;
 
 }
